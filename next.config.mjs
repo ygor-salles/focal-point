47b6a3804ce5d0f3a3140f1d0ab9bdc81c068a5c
Ygor Salles <ygorsallesaniceto@gmail.com>
2024-09-19 22:12:30 -0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: false,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
